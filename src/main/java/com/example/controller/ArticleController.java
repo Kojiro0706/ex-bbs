073ecbc0777cf8503db25003178a29bdf89f9a3d
Kojiro0706 <s94.ogi.kojiro@gmail.com>
2021-08-18 19:16:36 +0900

    コメント投稿のhtmlの記述

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -14,7 +14,6 @@ import com.example.form.ArticleForm;
 import com.example.repository.ArticleRepository;
 import com.example.repository.CommentRepository;
 
-
 /**
  * 記事情報とコメント情報を操作するコントローラークラス.
  * 
@@ -27,11 +26,10 @@ public class ArticleController {
 
 	@Autowired
 	private ArticleRepository articleRepository;
-	
+
 	@Autowired
 	private CommentRepository commentRepository;
-	
-	
+
 	/**
 	 * 記事情報を一覧で表示する.
 	 * 
@@ -41,30 +39,39 @@ public class ArticleController {
 	@RequestMapping("")
 	public String index(Model model) {
 		List<Article> articleList = articleRepository.findAll();
-		for(Article article:articleList) {
+		for (Article article : articleList) {
 			int articleId = article.getId();
 			List<Comment> commentList = commentRepository.findByArticleId(articleId);
 			article.setCommentList(commentList);
 		}
 		model.addAttribute("articleList", articleList);
-		
+
 		return "article-list";
 	}
-	
+
 	/**
 	 * 記事情報の登録をする.
 	 * 
 	 * @param form
-	 * @return　記事情報
+	 * @return 記事情報
 	 */
 	@RequestMapping("/insertArticle")
 	public String insertArticle(ArticleForm form) {
 		Article article = new Article();
 		BeanUtils.copyProperties(form, article);
 		articleRepository.insert(article);
+
+		return "redirect:/article";
+	}
+
+	
+	@RequestMapping("/insertComment")
+	public String insertComment(Comment form) {
+		Comment comment = new Comment();
+		BeanUtils.copyProperties(form, comment);
+		
+		commentRepository.insert(comment);
 		
 		return "redirect:/article";
 	}
-	
-	
 }
--- src/main/java/com/example/domain/Comment.java
+++ src/main/java/com/example/domain/Comment.java
@@ -15,55 +15,49 @@ public class Comment {
 	/** コンテント */
 	private String content;
 	/** 記事ID */
-	private Integer articledId;
-
+	private Integer articleId;
+	
 	public Comment() {
-
+		
 	}
-
-	public Comment(Integer id, String name, String content, Integer articledID) {
+	public Comment(Integer id, String name, String content, Integer articleId) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.content = content;
-		this.articledId = articledID;
+		this.articleId = articleId;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getContent() {
 		return content;
 	}
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
-	public Integer getArticledId() {
-		return articledId;
+	public Integer getArticleId() {
+		return articleId;
 	}
-
-	public void setArticledId(Integer articledId) {
-		this.articledId = articledId;
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
 	}
-
 	@Override
 	public String toString() {
-		return "Comment [id=" + id + ", name=" + name + ", content=" + content + ", articledId=" + articledId + "]";
+		return "Comment [id=" + id + ", name=" + name + ", content=" + content + ", articleId=" + articleId + "]";
 	}
+	
+	
+	
+	
 
 }
