ff3b87ab89957662926686228e48003280e09de9
Kojiro0706 <s94.ogi.kojiro@gmail.com>
2021-08-18 14:00:28 +0900

    rticleFormの作成

--- src/main/java/com/example/form/ArticleForm.java
+++ src/main/java/com/example/form/ArticleForm.java
@@ -1,5 +1,38 @@
 package com.example.form;
 
+
+/**
+ * 記事情報投稿時に使用するフォームクラス.
+ * 
+ * @author kojiro0706
+ *
+ */
 public class ArticleForm {
+
+	/** 名前　*/
+	private String name;
+	/** コンテント　*/
+	private String content;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleForm [name=" + name + ", content=" + content + "]";
+	}
 
 }
